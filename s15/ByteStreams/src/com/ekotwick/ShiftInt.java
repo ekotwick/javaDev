a92ffccc92f7c695015e6058d5baf9107ca94c5f
ekotwick <edmond.kotwick@gmail.com>
2017-07-27 12:08:57 -0400

    add ShiftInt class to illustrate bitshift operation

---
+++ s15/ByteStreams/src/com/ekotwick/ShiftInt.java
@@ -0,0 +1,38 @@
+package com.ekotwick;
+
+/**
+ * Created by ekotwick on 7/27/17.
+ */
+public class ShiftInt {
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001b[35m";
+    private static final String ANSI_CYAN = "\u001b[36m";
+    private static final String ANSI_WHITE = "\u001B[37m";
+
+    public static void main(String[] args) {
+        int x = 922342959;
+
+        writeInt(x);
+    }
+
+    public static void display(int x) {
+        String all = String.format("%32s", Integer.toBinaryString(x)).replace(" ", "0");
+        String coloredBinary = all.substring(0, 24) + ANSI_PURPLE + all.substring(24) + ANSI_RESET;
+        int y = x & 0xFF;
+        String output = String.format("%10d and 0xFF is %8s \t %10d is ", y, Integer.toBinaryString(y), x) + coloredBinary;
+        System.out.println(output);
+    }
+
+    private static void writeInt(int v) {
+        int x;
+        display(v >>> 24);
+        display(v >>> 16);
+        display(v >>> 8);
+        display(v >>> 0);
+    }
+}
