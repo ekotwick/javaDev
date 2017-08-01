6dc92cee05f63bb505c7dc5c5a9e0334700913f6
ekotwick <edmond.kotwick@gmail.com>
2017-08-01 10:00:17 -0400

    add deadlocking code (for learning purposes)

--- s16/Messages/src/com/ekotwick/Main.java
+++ s16/Messages/src/com/ekotwick/Main.java
@@ -6,7 +6,9 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
     }
 }
 
@@ -60,5 +62,25 @@ class Writer implements Runnable {
         }
 
         message.write("Finished");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+        for(String latestMessage = message.read(); !latestMessage.equals("Finished"); latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch(InterruptedException e) {
+
+            }
+        }
     }
 }
