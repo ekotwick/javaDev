9b078416a071ae835d6c8ff24a4d8622ebb6ad4f
ekotwick <edmond.kotwick@gmail.com>
2017-08-01 09:19:13 -0400

    initialize synch exercise

---
+++ s16/Messages/src/com/ekotwick/Main.java
@@ -0,0 +1,9 @@
+package com.ekotwick;
+
+// this application will have two threads, one that creates messages and one that consumes messages
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
