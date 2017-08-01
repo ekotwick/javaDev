b0639fada36a53f4ae0b28c614d8a3d494b79e47
ekotwick <edmond.kotwick@gmail.com>
2017-08-01 10:53:49 -0400

    add Consumer class

--- s16/ProducerConsumer/src/com/ekotwick/Main.java
+++ s16/ProducerConsumer/src/com/ekotwick/Main.java
@@ -1,20 +1,31 @@
 package com.ekotwick;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static com.ekotwick.Main.EOF;
+
 public class Main {
+    public static final String EOF = "EOF";
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> buffer = new ArrayList<>();
+        MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_GREEN);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE);
+        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_BLUE);
+
+        new Thread(producer).start();
+        new Thread(consumer1).start();
+        new Thread(consumer2).start();
     }
 }
 
-class myProducer implements Runnable {
+class MyProducer implements Runnable {
     private List<String> buffer;
     private String color;
 
-    public myProducer(List<String> buffer, String color) {
+    public MyProducer(List<String> buffer, String color) {
         this.buffer = buffer;
         this.color = color;
     }
@@ -37,5 +48,31 @@ class myProducer implements Runnable {
 
         System.out.println(color + "Adding EOF and exiting...");
         buffer.add("EOF");
+    }
+}
+
+class MyConsumer implements Runnable {
+    private List<String> buffer;
+    private String color;
+
+    public MyConsumer(List<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    // this is a simplification: typically, we would have the method wait for a notifaction rather than run continuously like this; but this is a contrived example and designed for simplicity's sake
+    @Override
+    public void run() {
+        while(true) {
+            if(buffer.isEmpty()) {
+                continue;
+            }
+            if(buffer.get(0).equals(EOF)) {
+                System.out.println(color+"Exiting");
+                break;
+            } else {
+                System.out.println(color + "Removed " + buffer.remove(0));
+            }
+        }
     }
 }
