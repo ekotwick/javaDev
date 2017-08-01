3f9b1470e147ec98aad2a0c4c5b244c057cd91c4
ekotwick <edmond.kotwick@gmail.com>
2017-08-01 14:39:40 -0400

    initialize executiveService interface exercise

---
+++ s16/ExecutiveService/src/com/ekotwick/Main.java
@@ -0,0 +1,99 @@
+package com.ekotwick;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static com.ekotwick.Main.EOF;
+
+public class Main {
+    public static final String EOF = "EOF";
+
+    public static void main(String[] args) {
+        List<String> buffer = new ArrayList<>();
+        ReentrantLock bufferLock = new ReentrantLock();
+        MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_GREEN, bufferLock);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
+        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_BLUE, bufferLock);
+
+        new Thread(producer).start();
+        new Thread(consumer1).start();
+        new Thread(consumer2).start();
+    }
+}
+
+class MyProducer implements Runnable {
+    private List<String> buffer;
+    private String color;
+    private ReentrantLock bufferLock;
+
+    public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
+        this.buffer = buffer;
+        this.color = color;
+        this.bufferLock = bufferLock;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        String[] nums = {"1", "2", "3", "4", "5"};
+
+        for(String num : nums) {
+            try {
+                System.out.println(color + "Adding..." + num);
+                bufferLock.lock();
+                try {
+                    buffer.add(num);
+                } finally {
+                    bufferLock.unlock();
+                }
+                Thread.sleep(random.nextInt(1000));
+            } catch(InterruptedException e) {
+                System.out.println("producer was interrupted");
+            }
+        }
+
+        System.out.println(color + "Adding EOF and exiting...");
+        bufferLock.lock();
+        try {
+            buffer.add("EOF");
+        } finally {
+            bufferLock.unlock();
+        }
+    }
+}
+
+class MyConsumer implements Runnable {
+    private List<String> buffer;
+    private String color;
+    private ReentrantLock bufferLock;
+
+    public MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
+        this.buffer = buffer;
+        this.color = color;
+        this.bufferLock = bufferLock;
+    }
+
+    // this is a simplification: typically, we would have the method wait for a notifaction rather than run continuously like this; but this is a contrived example and designed for simplicity's sake
+    @Override
+    public void run() {
+        while(true) {
+            // we don't the producer/consumer to change the arraylist once a consumer thread has checked to see whether it is empty; so we want all method calls to the ArrayList to happen as a unit and at once in this `critical section`.
+            bufferLock.lock();
+            try {
+                if(buffer.isEmpty()) {// we need other `bufferLock.lock()` calls here; if the buffer is empty, we hit the `continue` to the next loop, where another `lock()` call is made, and so on with no releases of the lock; same with the `break`: we have locked the lock without ever releasing it.
+                    continue;
+                }
+                if(buffer.get(0).equals(EOF)) {
+                    System.out.println(color+"Exiting");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
+            } finally {
+                bufferLock.unlock();
+            }
+        }
+    }
+}
